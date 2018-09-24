671d7c5ee7f91e92636138fac256223be66da72c
soapsabee <s5930213055@phuket.psu.ac.th>
2018-09-24 11:17:17 +0700

    ver1.0

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var pgp = require('pg-promise')();
 var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
 //app.use(express.static('www'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', function(req, res) {
     res.render('pages/index');
@@ -38,6 +41,51 @@ app.get('/products', function(req, res) {
 
     
 });
+
+
+app.get('/products/:pid', function(req, res) {
+    
+    var pid = req.params.pid;
+    var sql = "select * from products where id=" + pid;
+
+    db.any(sql, )
+    .then(function(data){
+        console.log('data' + data);
+        res.render('pages/product_edit',{product : data[0]});
+    })
+    .catch(function(error){
+
+        console.log('ERROR'+ error);
+
+    })
+
+});
+
+//update data
+app.post('/products/update', function(req, res) {
+    
+    var id = req.body.id;
+    var title = req.body.title;
+    var price = req.body.price;
+    var sql = `update product set title= ${title}, price= ${price} where id = ${id}`;
+    res.send(sql);
+    db.none(sql, )
+    //db.none
+    .then(function(data){
+        console.log('data' + data);
+        res.redirect('/products');
+       // res.render('pages/products',{products : data});
+    })
+    .catch(function(error){
+
+        console.log('ERROR'+ error);
+
+    })
+
+});
+
+
+
 
 app.get('/users/:id', function(req, res) {
     var id = req.param('id');
